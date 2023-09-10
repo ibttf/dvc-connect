3b3b365a84fbe7ab99754a9e70aa644c439df48a
roylee0912 <leeroy0912@gmail.com>
2023-09-09 17:00:10 -0700

    fixed time bug not showing am/pm correctly, navbar spacing, subject change not switching topic

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -30,63 +30,65 @@ const Navbar = (props) => {
 
 
     return (
-
-    <nav className="lg:text-lg md:text-md text-xxs flex justify-between lg:py-6 lg:px-4 py-3 px-2 border-b-4 border-green-600" onClick={()=>setIsLanguageDropdownVisible(false)} style={{width: "100%"}}>
-        <div className="flex items-center">
-            <Link to="/" className=" font-extrabold text-green-600 cursor-pointer">
-                DVC Connect
-            </Link>
-            <div className="relative mx-0 z-20">
-                <div 
-                    className={`text-md cursor-pointer px-2 font-semibold duration-100 hover:underline ${isLanguageDropdownVisible ? "underline": ""}`}
-                    onClick={(e) => {e.stopPropagation(); setIsLanguageDropdownVisible(!isLanguageDropdownVisible)}}
-                >
-                    {displayLanguage(props.language)}
-                    
-                </div>
-                {isLanguageDropdownVisible && (
-                    <div className="absolute top-full mt-2 w-max border rounded bg-white">
-                        {props.language !== "chinese" && (
-                            <div onClick={() => {props.setLanguage("chinese"); setIsLanguageDropdownVisible(false);}} className="flex items-center md:p-6 p-3 hover:bg-green-50 cursor-pointer font-semibold duration-100">
-                                <img className="md:w-6 md:h-4 w-3 h-2 md:mr-2 mr-0.5" src={china} />Chinese
-                            </div>
-                        )}
-                        {props.language !== "korean" && (
-                            <div onClick={() => {props.setLanguage("korean"); setIsLanguageDropdownVisible(false);}} className="flex items-center md:p-6 p-3 hover:bg-green-50 cursor-pointer font-semibold duration-100">
-                                <img className="md:w-6 md:h-4 w-3 h-2 md:mr-2 mr-0.5" src={korea} />Korean
-                            </div>
-                        )}
-                        {props.language !== "spanish" && (
-                            <div onClick={() => {props.setLanguage("spanish"); setIsLanguageDropdownVisible(false);}} className="flex items-center md:p-6 p-3 hover:bg-green-50 cursor-pointer font-semibold duration-100">
-                                <img className="md:w-6 md:h-4 w-3 h-2 md:mr-2 mr-0.5" src={mexico} />Spanish
-                            </div>
-                        )}
-                        {props.language !== "english" && (
-                            <div onClick={() => {props.setLanguage("english"); setIsLanguageDropdownVisible(false);}} className="flex items-center md:p-6 p-3 hover:bg-green-50 cursor-pointer font-semibold duration-100">
-                                <img className="md:w-6 md:h-4 w-3 h-2 md:mr-2 mr-0.5" src={us} />English
-                            </div>
-                        )}
+    <div className="border-b-4 border-green-600" onClick={()=>setIsLanguageDropdownVisible(false)}>
+        <nav className="md:w-11/12 w-full mx-auto lg:text-lg md:text-md text-xxs flex justify-between lg:py-6 lg:px-4 py-3 px-2 ">
+            
+            <div className="flex items-center">
+                <Link to="/" className=" font-extrabold text-green-600 cursor-pointer">
+                    DVC Connect
+                </Link>
+                <div className="relative mx-0 z-20">
+                    <div 
+                        className={`text-md cursor-pointer px-2 font-semibold duration-100 hover:underline ${isLanguageDropdownVisible ? "underline": ""}`}
+                        onClick={(e) => {e.stopPropagation(); setIsLanguageDropdownVisible(!isLanguageDropdownVisible)}}
+                    >
+                        {displayLanguage(props.language)}
+                        
                     </div>
-                )}
+                    {isLanguageDropdownVisible && (
+                        <div className="absolute top-full mt-2 w-max border rounded bg-white">
+                            {props.language !== "chinese" && (
+                                <div onClick={() => {props.setLanguage("chinese"); setIsLanguageDropdownVisible(false);}} className="flex items-center md:p-6 p-3 hover:bg-green-50 cursor-pointer font-semibold duration-100">
+                                    <img className="md:w-6 md:h-4 w-3 h-2 md:mr-2 mr-0.5" src={china} />Chinese
+                                </div>
+                            )}
+                            {props.language !== "korean" && (
+                                <div onClick={() => {props.setLanguage("korean"); setIsLanguageDropdownVisible(false);}} className="flex items-center md:p-6 p-3 hover:bg-green-50 cursor-pointer font-semibold duration-100">
+                                    <img className="md:w-6 md:h-4 w-3 h-2 md:mr-2 mr-0.5" src={korea} />Korean
+                                </div>
+                            )}
+                            {props.language !== "spanish" && (
+                                <div onClick={() => {props.setLanguage("spanish"); setIsLanguageDropdownVisible(false);}} className="flex items-center md:p-6 p-3 hover:bg-green-50 cursor-pointer font-semibold duration-100">
+                                    <img className="md:w-6 md:h-4 w-3 h-2 md:mr-2 mr-0.5" src={mexico} />Spanish
+                                </div>
+                            )}
+                            {props.language !== "english" && (
+                                <div onClick={() => {props.setLanguage("english"); setIsLanguageDropdownVisible(false);}} className="flex items-center md:p-6 p-3 hover:bg-green-50 cursor-pointer font-semibold duration-100">
+                                    <img className="md:w-6 md:h-4 w-3 h-2 md:mr-2 mr-0.5" src={us} />English
+                                </div>
+                            )}
+                        </div>
+                    )}
+                </div>
             </div>
-        </div>
-        <div className={`w-fit grid grid-cols-2 md:gap-4 gap-1 items-center`}>
-            <Link to="/about" className="font-bold hover:underline text-green-600">
-                About
-            </Link>
-            {currentUser ? 
-            <button onClick={()=>{signOut(auth); localStorage.clear()}} className="font-bold hover:underline text-green-600">
-                Logout
-                
-            </button>
-            :
-            <Link to="/tutor-login" className="font-bold hover:underline text-green-600">
-                TutorZone
-            </Link>
-            }
-           
-        </div>
-    </nav>
+            <div className={`grid grid-cols-2 md:gap-4 gap-1 items-center`}>
+                <Link to="/about" className="font-bold hover:underline text-green-600">
+                    About
+                </Link>
+                {currentUser ? 
+                <button onClick={()=>{signOut(auth); localStorage.clear()}} className="font-bold hover:underline text-green-600">
+                    Logout
+                    
+                </button>
+                :
+                <Link to="/tutor-login" className="font-bold hover:underline text-green-600">
+                    TutorZone
+                </Link>
+                }
+            
+            </div>
+        </nav>
+    </div>
 
 );
     
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -23,7 +23,16 @@ const Home = (props) => {
 };
 
 
+
   const [userHasMatchingDoc, setUserHasMatchingDoc] = useState(false);
+
+
+  function formatHour(hour) {
+    const intHour = parseInt(hour);
+    const suffix = (intHour >= 900 && intHour < 1200) ? ' AM' : ' PM';
+    return hour.slice(0,2) + ":" + hour.slice(2) + suffix;
+  }
+
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged(async (user) => {
@@ -115,42 +124,49 @@ const Home = (props) => {
             </div>
 
 
+
             <div className="grid grid-cols-4 items-center bg-white lg:py-4 py-1 px-8 rounded-lg">
-                <h1 className="lg:text-lg md:text-md text-xs font-semibold text-right col-span-1 flex w-full ">
-                    During:
-                </h1>
-                <div className="col-span-3">
-                    <div className="relative">
-                        <div 
-                            className="lg:text-lg md:text-md text-xs text-gray-900 block w-full py-3 px-4 pr-8 rounded leading-tight cursor-pointer"
-                            onClick={(e) => {e.stopPropagation(); setIsDayOfTheWeekDropdownVisible(false); setIsHoursDropdownVisible(!isHoursDropdownVisible);}}
-                        >
-                            {hours.slice(0,2) + ":" + hours.slice(2) + (parseInt(hours) >= 1200 ? ' PM' : ' AM')}
-                            <span className="absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                                <svg className={`fill-current h-4 w-4 ${isHoursDropdownVisible ? "rotate-180" : ""} duration-300`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                    <path d="M5.293 9.293L10 14l4.707-4.707a.999.999 0 0 0 0-1.414l-.707-.707a.999.999 0 0 0-1.414 0L10 11.586 7.414 8.172a.999.999 0 0 0-1.414 0l-.707.707a.999.999 0 0 0 0 1.414z" />
-                                </svg>
-                            </span>
-                        </div>
-                        {isHoursDropdownVisible && (
-                            <div className="absolute border border-green-600 mx-auto grid md:grid-cols-4 grid-cols-2 gap-0 w-max rounded mt-5 bg-white z-10 text-gray-600 whitespace-nowrap lg:text-lg md:text-md text-xxs">
-                                {["0900", "0930", "1000", "1030", "1100", "1130", "1200", "1230", "0100", "0130", "0200", "0230", "0300", "0330", "0400", "0430"].map(hour => (
-                                    <div
-                                        key={hour}
-                                        className="py-2 px-4 hover:bg-gray-100 cursor-pointer text-center"
-                                        onClick={() => {
-                                            setHours(hour);
-                                            setIsHoursDropdownVisible(false);
-                                        }}
-                                    >
-                                        {hour.slice(0,2) + ":" + hour.slice(2) + (parseInt(hour) >= 1200 ? ' PM' : ' AM')}
-                                    </div>
-                                ))}
-                            </div>
-                        )}
-                    </div>
-                </div>
-            </div>
+              <h1 className="lg:text-lg md:text-md text-xs font-semibold text-right col-span-1 flex w-full ">
+                  During:
+              </h1>
+              <div className="col-span-3">
+                  <div className="relative">
+                      <div 
+                          className="lg:text-lg md:text-md text-xs text-gray-900 block w-full py-3 px-4 pr-8 rounded leading-tight cursor-pointer"
+                          onClick={(e) => {e.stopPropagation(); setIsDayOfTheWeekDropdownVisible(false); setIsHoursDropdownVisible(!isHoursDropdownVisible);}}
+                      >
+                          {formatHour(hours)}
+                          <span className="absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                              <svg className={`fill-current h-4 w-4 ${isHoursDropdownVisible ? "rotate-180" : ""} duration-300`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                  <path d="M5.293 9.293L10 14l4.707-4.707a.999.999 0 0 0 0-1.414l-.707-.707a.999.999 0 0 0-1.414 0L10 11.586 7.414 8.172a.999.999 0 0 0-1.414 0l-.707.707a.999.999 0 0 0 0 1.414z" />
+                              </svg>
+                          </span>
+                      </div>
+                      {isHoursDropdownVisible && (
+                          <div className="absolute border border-green-600 mx-auto grid md:grid-cols-4 grid-cols-2 gap-0 w-max rounded mt-5 bg-white z-10 text-gray-600 whitespace-nowrap lg:text-lg md:text-md text-xxs">
+                              {["0900", "0930", "1000", "1030", "1100", "1130", "1200", "1230", "0100", "0130", "0200", "0230", "0300", "0330", "0400", "0430"].map(hour => (
+                                  <div
+                                      key={hour}
+                                      className="py-2 px-4 hover:bg-gray-100 cursor-pointer text-center"
+                                      onClick={() => {
+                                          setHours(hour);
+                                          setIsHoursDropdownVisible(false);
+                                      }}
+                                  >
+                                      {formatHour(hour)}
+                                  </div>
+                              ))}
+                          </div>
+                      )}
+                  </div>
+              </div>
+          </div>
+
+
+
+
+
+
 
             <div className="grid grid-cols-4 items-center bg-white lg:py-4 py-1 px-8 rounded-lg">
               <h1 className="lg:text-lg md:text-md text-xs font-semibold text-right col-span-1 flex w-full">
@@ -179,6 +195,7 @@ const Home = (props) => {
                                   key={subject}
                                   className="py-2 px-4 hover:bg-gray-100 cursor-pointer"
                                   onClick={() => {
+                                      setSelectedTopic("Any")
                                       setSelectedSubject(subject);
                                       setIsSubjectDropdownVisible(false);
                                   }}
